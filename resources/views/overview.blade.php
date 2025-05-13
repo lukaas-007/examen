485f9f8b3337df4c7fedce9ebedf565de0806848
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-05-13 13:23:36 +0200

    temptable with temp time

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -9,6 +9,31 @@
     @vite(['resources/css/app.css', 'resources/css/add_wolf.css'])
     {{ __('overview.overview') }}
 
+    <table>
+        <thead>
+            <tr>
+                <th>{{ __('overview.date_spotted') }}</th>
+                <th>{{ __('overview.area_spotted') }}</th>
+                <th>{{ __('overview.spotted_amount') }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php $wolfs = [
+                (object) ['date_spotted' => '2023-10-01', 'area_spotted' => 'Area 1', 'spotted_amount' => 5],
+                (object) ['date_spotted' => '2023-10-02', 'area_spotted' => 'Area 2', 'spotted_amount' => 3],
+                (object) ['date_spotted' => '2023-10-03', 'area_spotted' => 'Area 3', 'spotted_amount' => 7],
+            ]; @endphp
+
+            @foreach ($wolfs as $wolf)
+                <tr>
+                    <td>{{ $wolf->date_spotted }}</td>
+                    <td>{{ $wolf->area_spotted }}</td>
+                    <td>{{ $wolf->spotted_amount }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
     <a href="{{ route('add_wolf') }}">
         {{ __('add_wolf.title') }}
     </a>
