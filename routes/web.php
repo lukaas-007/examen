cf71a3ebb30270f94295c8074a2328c3ef1ef3da
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-05-13 12:16:30 +0200

    init

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('add_wolf');
+});
+
+Route::get('/overview', function () {
+    return view('overview');
 });
