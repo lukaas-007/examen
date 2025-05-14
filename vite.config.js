cdd9601957cf82ba4eaf4a912af3abd95408250f
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-05-14 09:30:46 +0200

    update config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/css/add_wolf.css',
+                'resources/css/overview.css', 
+                'resources/js/app.js',
+                'resources/js/add_wolf.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
